2c8479a953519dd6f41c9ab799b28997d4a07041
Szymon Biliński <szymon.bilinski@gmail.com>
2015-07-06 19:13:43 +0200

    Improved test fixtures.

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -7,24 +7,53 @@ marketPulseControllers.controller('ExchangeStatsCtrl', ['$scope', 'ExchangeStats
 
 
 marketPulseControllers.controller('TradePublisherCtrl', ['$scope', 'Trade', function($scope, Trade) {
+    var fixedRates = {
+        'EUR': {
+            'EUR': 1.0,
+            'GBP': 0.71,
+            'PLN': 4.15
+        },
+        'GBP': {
+            'EUR': 1.41,
+            'GBP': 1.0,
+            'PLN': 5.92
+        },
+        'PLN': {
+            'EUR': 0.24,
+            'GBP': 0.17,
+            'PLN': 1.0
+        }
+    };
+
+    $scope.countries = ['UK', 'PL']
+    $scope.currencies = Object.keys(fixedRates);
+
     $scope.trade = {
-      userId: "123456",
-      currencyFrom: "EUR",
-      currencyTo: "GBP",
+      userId: Math.floor(Math.random() * 1000000).toString(),
+      currencyFrom: $scope.currencies[1],
+      currencyTo: $scope.currencies[2],
       amountSell: 1000.0,
       amountBuy: 710.0,
       rate: 0.71,
-      timePlaced: "27-JUN-15 18:15:10",
-      originatingCountry: "UK"
+      timePlaced: (new Date()).toString('dd-MMM-yyyy HH:mm:ss'),
+      originatingCountry: $scope.countries[0]
     };
 
-    $scope.onRateChange = function(r) {
-        $scope.trade.amountBuy = Math.round($scope.trade.amountSell * r * 100) / 100;
-    }
+    $scope.$watch('trade.rate', function(newvalue, oldvalue) {
+        $scope.trade.amountBuy = Math.round($scope.trade.amountSell * newvalue * 100) / 100;
+    });
 
-    $scope.onAmountSellChange = function(a) {
-        $scope.trade.amountBuy = Math.round(a * $scope.trade.rate * 100) / 100;
-    }
+    $scope.$watch('trade.amountSell', function(newvalue, oldvalue) {
+        $scope.trade.amountBuy = Math.round(newvalue * $scope.trade.rate * 100) / 100;
+    });
+
+    $scope.$watch('trade.currencyFrom', function(newvalue, oldvalue) {
+        $scope.trade.rate = fixedRates[newvalue][$scope.trade.currencyTo];
+    });
+
+    $scope.$watch('trade.currencyTo', function(newvalue, oldvalue) {
+        $scope.trade.rate = fixedRates[$scope.trade.currencyFrom][newvalue];
+    });
 
     $scope.publishTrade = function() {
         Trade.create($scope.trade);
